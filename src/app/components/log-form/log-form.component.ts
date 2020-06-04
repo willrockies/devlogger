58dcf02f33d12bce60680ee750f1e28be5a3ac77
Wilson Alves <wlsn.ads@gmail.com>
2020-06-04 00:34:07 -0300

    Update & delete logs

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -14,18 +14,52 @@ export class LogFormComponent implements OnInit {
   id: string;
   text: string;
   date: any;
+
+  isNew: boolean = true;
+
   constructor(private logService:LogService) { }
 
   ngOnInit() {
   // Subscribe to the selectedLog observable
   this.logService.selectedLog.subscribe(log => {
     if(log.id !== null){
+      this.isNew = false;
       this.id = log.id;
       this.text = log.text;
       this.date = log.date;
     }
   })
-
   }
+  onSubmit(){
+    // Check if new lof
+
+    if(this.isNew){
+      // create a new log
+      const newLog = {
+        id: this.generateId(),
+        text: this.text,
+        date: new Date()
+      }
+      //Add Log
+      this.logService.addLog(newLog)
+    }else{
+      // Create log to be updated
+      const updLog = {
+        id: this.id,
+        text: this.text,
+        date: new Date()
+      }
+      //Update log
+      this.logService.updateLog(updLog);
+    }
+  }
+  generateId() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+
+
 
 }
